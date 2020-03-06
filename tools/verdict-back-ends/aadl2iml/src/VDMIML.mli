3d8ec514bc40e61b4ae121b3f515be9a94b6a890
Daniel Larraz <daniel-larraz@users.noreply.github.com>
2020-03-06 09:54:25 -0600

    Remove Apache license. Apply BSD 3-Clause License

--- tools/verdict-back-ends/aadl2iml/src/VDMIML.mli
+++ tools/verdict-back-ends/aadl2iml/src/VDMIML.mli
@@ -1,20 +1,14 @@
-(* Copyright (c) 2019 by the Board of Trustees of the University of Iowa
+(* Copyright (c) 2019-2020, Board of Trustees of the University of Iowa.
+   Copyright (c) 2019-2020, General Electric Company.
+   All rights reserved.
 
-   Licensed under the Apache License, Version 2.0 (the "License"); you
-   may not use this file except in compliance with the License.  You
-   may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0 
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-   implied. See the License for the specific language governing
-   permissions and limitations under the License. 
-
+   Use of this source code is governed by a BSD 3-Clause License that
+   can be found in the LICENSE file.
 *)
 
-(** @author Daniel Larraz *)
+(** @author Daniel Larraz
+    @author William D. Smith
+*)
 
 type identifier = string
 
--- tools/verdict-back-ends/aadl2iml/src/VerdictAst.mli
+++ tools/verdict-back-ends/aadl2iml/src/VerdictAst.mli
@@ -1,3 +1,14 @@
+(* Copyright (c) 2019-2020, General Electric Company.
+   Copyright (c) 2019-2020, Board of Trustees of the University of Iowa.
+   All rights reserved.
+
+   Use of this source code is governed by a BSD 3-Clause License that
+   can be found in the LICENSE file.
+*)
+
+(** @author William D. Smith
+    @author Daniel Larraz
+*)
 
 type cia = CIA_C | CIA_I | CIA_A
 
