8052033b596b2d96f3a5c7cab8b10301e6e4e712
William Smith <william.D.smith@ge.com>
2020-06-19 10:20:22 -0400

    add timing

--- tools/verdict-back-ends/verdict-bundle-parent/verdict-synthesis/src/main/java/com/ge/verdict/synthesis/App.java
+++ tools/verdict-back-ends/verdict-bundle-parent/verdict-synthesis/src/main/java/com/ge/verdict/synthesis/App.java
@@ -2,17 +2,15 @@ package com.ge.verdict.synthesis;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Supplier;
 
 import com.ge.verdict.attackdefensecollector.AttackDefenseCollector;
 import com.ge.verdict.attackdefensecollector.AttackDefenseCollector.Result;
 import com.ge.verdict.attackdefensecollector.CSVFile.MalformedInputException;
-import com.ge.verdict.synthesis.dtree.DAnd;
 import com.ge.verdict.synthesis.dtree.DLeaf;
-import com.ge.verdict.synthesis.dtree.DOr;
 import com.ge.verdict.synthesis.dtree.DTree;
 
 public class App {
@@ -23,46 +21,48 @@ public class App {
      * in the Docker image.
      */
 
-	public static void main(String[] args) throws IOException, MalformedInputException {
+	public static void main(String[] args) {
 		if (args.length < 2) {
 			throw new RuntimeException("Must specify STEM output directory and cost model XML!");
         }
 
+		long startTime = System.currentTimeMillis();
+
 		String stemOutDir = args[0];
 		String costModelXml = args[1];
 
-		CostModel costModel = new CostModel(new File(costModelXml));
+		final CostModel costModel = timed("Load cost model", () -> new CostModel(new File(costModelXml)));
 
-		AttackDefenseCollector collector = new AttackDefenseCollector(stemOutDir);
-		List<Result> results = collector.perform();
+		AttackDefenseCollector collector = timed("Load CSV", () -> {
+			try {
+				return new AttackDefenseCollector(stemOutDir);
+			} catch (IOException | MalformedInputException e) {
+				throw new RuntimeException(e);
+			}
+		});
+		List<Result> results = timed("Build attack-defense tree", () -> collector.perform());
 
 		for (Result result : results) {
 			System.out.println();
 			System.out.println("Result for cyber req: " + result.cyberReq.getName());
-			DTree dtree = DTreeConstructor.construct(result.adtree, costModel, result.cyberReq.getSeverityDal());
-			Optional<Set<DLeaf>> selected = VerdictSynthesis.performSynthesis(dtree);
+			DTree dtree = timed("Construct defense tree",
+					() -> DTreeConstructor.construct(result.adtree, costModel, result.cyberReq.getSeverityDal()));
+			Optional<Set<DLeaf>> selected = timed("Perform synthesis", () -> VerdictSynthesis.performSynthesis(dtree));
 			if (selected.isPresent()) {
 				for (DLeaf leaf : selected.get()) {
 					System.out.println("Selected leaf: " + leaf.prettyPrint());
 				}
 			}
 		}
+
+		System.out.println(" == Total time: " + (System.currentTimeMillis() - startTime) + " milliseconds");
 	}
 
-	private static DTree buildDemo() {
-        return new DOr(
-                Arrays.asList(
-                        new DAnd(
-                                Arrays.asList(
-                                        DLeaf.createIfNeeded("A-comp", "A-def", "A-att", 1),
-                                        DLeaf.createIfNeeded("B-comp", "B-def", "B-att", 1))),
-                        new DAnd(
-                                Arrays.asList(
-                                        DLeaf.createIfNeeded("B-comp", "B-def", "B-att", 1),
-                                        DLeaf.createIfNeeded("C-comp", "C-def", "C-att", 1))),
-                        new DAnd(
-                                Arrays.asList(
-                                        DLeaf.createIfNeeded("A-comp", "A-def", "A-att", 1),
-                                        DLeaf.createIfNeeded("C-comp", "C-def", "C-att", 1)))));
-    }
+	public static <T> T timed(String title, Supplier<T> function) {
+		long startTime = System.currentTimeMillis();
+		T ret = function.get();
+		System.out.println(
+				" == " + title + ", time: " + (System.currentTimeMillis() - startTime) + " milliseconds");
+		return ret;
+	}
 }
