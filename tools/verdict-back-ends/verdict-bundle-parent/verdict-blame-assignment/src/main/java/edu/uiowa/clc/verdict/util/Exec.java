b078730fec9d9be5612c5d5635f8f15218f34c06
M. Fareed Arif <fareed.arif@yahoo.com>
2020-08-05 18:28:17 -0500

    updated crv to test the change

--- tools/verdict-back-ends/verdict-bundle-parent/verdict-blame-assignment/src/main/java/edu/uiowa/clc/verdict/util/Exec.java
+++ tools/verdict-back-ends/verdict-bundle-parent/verdict-blame-assignment/src/main/java/edu/uiowa/clc/verdict/util/Exec.java
@@ -29,7 +29,7 @@ import org.apache.tools.ant.taskdefs.PumpStreamHandler;
 
 public class Exec {
 
-    public static int run_kind2(File lustureFile, File kind2ResultFile) throws IOException {
+    public static int run_kind2(File lustureFile, File kind2ResultFile, boolean emptySelection) throws IOException {
 
         //        InputStream resultStream = null;
         final String KIND2 = "kind2";
@@ -69,12 +69,17 @@ public class Exec {
 
         File kind2 = new File(user_dir, KIND2);
 
-        cmd_arg.add(kind2.getAbsolutePath());
-        cmd_arg.add("--enable");
-        cmd_arg.add("MCS");
-        cmd_arg.add("--print_mcs_legacy");
-        cmd_arg.add("true");
-
+        if(emptySelection) {
+        	cmd_arg.add("--ivc");
+        	cmd_arg.add("--ivc_category");
+        	cmd_arg.add("contracts");        	
+        } else {
+	        cmd_arg.add(kind2.getAbsolutePath());
+	        cmd_arg.add("--enable");
+	        cmd_arg.add("MCS");
+	        cmd_arg.add("--print_mcs_legacy");
+	        cmd_arg.add("true");
+        }
         cmd_arg.add(lustureFile.getAbsolutePath());
         cmd_arg.add("-xml");
 
