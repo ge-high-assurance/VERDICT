6e1af71b2e278f69b1ce74ac8299ddb0843c943b
Vidhya Tekken Valapil <vidhya.valapil@ge.com>
2020-11-16 11:03:57 -0500

    removed todo comments

--- tools/verdict/com.ge.research.osate.verdict/src/com/ge/research/osate/verdict/aadl2vdm/Aadl2Vdm.java
+++ tools/verdict/com.ge.research.osate.verdict/src/com/ge/research/osate/verdict/aadl2vdm/Aadl2Vdm.java
@@ -5075,11 +5075,8 @@ public class Aadl2Vdm {
 			dtype.setPlainType(verdict.vdm.vdm_data.PlainType.fromValue("int"));
 		} else if(aadlDataType.getName().contentEquals("Boolean")){
 			dtype.setPlainType(verdict.vdm.vdm_data.PlainType.fromValue("bool"));
-//TODO:remove		} else if (!(aadlDataType.getAllPropertyAssociations().isEmpty())){//if the dataType definition has properties
-//			dtype.setUserDefinedType(aadlDataType.getName());
 		} else {//not float or int or bool or enum
 			dtype.setUserDefinedType(aadlDataType.getName());
-			//System.out.println("Unresolved AADL Data type value is "+aadlDataType.getName());
 		}
 		defineDataType(aadlDataType, model, dataTypeDecl);
 		return dtype;
